f2da22ba8118a7de3b02d3f23518dda01dab09a3
cleitonts <cleitonsorrilha@gmail.com>
2020-12-11 15:56:38 -0300

    correção para rodar migrations com cd

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -6,6 +6,9 @@ declare -A REP
 
 if [ "$(ls -A /var/www/$APP)" ]; then
     # Sistema já existe
+    echo "------> Criando .env"
+    echo "DATABASE_URL='mysql://$DB_USER:$DB_PASS@$DB_HOST:3306/$DB_NAME'" > /var/www/${APP}/.env
+
     echo "------> Sistema pronto $APP"
 else
     # Sistema não existe
@@ -14,10 +17,16 @@ else
     git clone ${REP} /var/www/${APP}
 fi
 
+chmod a+rw -R /var/www/${APP}
+
 echo "------> Composer install $APP"
 cd /var/www/${APP}
 composer install --no-suggest --quiet
 
 echo "============> Instalação concluída $APP"
+
+echo "------> Doctrine migrations $APP"
+php bin/console doctrine:database:create --if-not-exists
+php bin/console --no-interaction doctrine:migrations:migrate
 
 php-fpm -R
